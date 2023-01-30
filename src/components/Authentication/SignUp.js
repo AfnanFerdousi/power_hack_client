05aa3de899f6bec15fd22b7c05b208309f1737a8
Afnan Ferdousi <afnanferdousi550@gmail.com>
2023-01-30 13:09:21 +0600

    link changed

--- src/components/Authentication/SignUp.js
+++ src/components/Authentication/SignUp.js
@@ -44,7 +44,7 @@ const SignUp = () => {
                     showConfirmButton: false,
                     timer: 1500
                 })
-                navigate('/billingpage')
+                window.location.href = '/billingpage'
 
             }
         } catch (err) {
